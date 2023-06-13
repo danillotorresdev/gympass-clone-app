8a9c45dde25da39d9c2c5ed1308a8d296b2d059a
danillotorresdev <danilloept@gmail.com>
2023-06-13 11:03:05 -0300

    test check-in controllers e2e

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -8,8 +8,8 @@ import { metrics } from '@/http/controllers/check-ins/metrics'
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
-  app.post('/check-ins/history', history)
-  app.post('/check-ins/metrics', metrics)
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
   app.patch('/check-ins/:checkInId/validate', validate)
